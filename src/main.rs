88643e028ebe808c18f27181530ba0201863b95e
Vishal <vishalchiluveri@gmail.com>
2023-05-01 17:49:38 +0530

    formatted main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ use rust_zero2prod::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8001").expect("Failed to bind address");
-    
+
     run(listener)?.await
 }
