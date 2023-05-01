f5652cf69209dfe3957c32678e771395322e82ce
Vishal <vishalchiluveri@gmail.com>
2023-05-01 17:42:16 +0530

    added health_check api and added tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, rust!");
+use std::net::TcpListener;
+
+use rust_zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8001").expect("Failed to bind address");
+    
+    run(listener)?.await
 }
